cd2fd1f3e59fae2bc62ad8208b1b8e5265eac450
Stoyan penchev <stoianpp2013@gmail.com>
2016-06-09 19:59:26 +0300

    User name added to registration view, view model and action

--- Web/AptSystems.Web/Controllers/AccountController.cs
+++ Web/AptSystems.Web/Controllers/AccountController.cs
@@ -159,7 +159,7 @@
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
